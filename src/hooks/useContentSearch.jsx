6a1081c8ca8596b56434b76f6da4aa8d816a68cc
snaider10 <snaiderortega10@gmail.com>
2025-11-13 00:05:20 -0500

    Add customs hook to find medie content

---
+++ src/hooks/useContentSearch.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import { apikey } from '../config/apiKey';
+export function UseMovieSearch({content}){
+    const [searchPintures, setSearchPintures] = useState([]);
+    useEffect(() => {
+        const findMovies = async() => {
+            try {
+               const movies = await fetch (`https://api.themoviedb.org/3/search/movie?query=${content}&include_adult=false&language=en-US&page=1&api_key=${apikey}`);
+               const data = await movies.json();
+               const date_names = data.results.map((movie) => ({ id: movie.id, title: movie.title }));
+               setSearchPintures(date_names);
+            } catch (error) {
+                console.error("Error fetching search data:", error);
+            }
+        }; 
+        findMovies();
+    },[content]);
+
+    return searchPintures;
+} 
+
+export function UseSeriesSearch({content}){
+    const [searchPintures, setSearchPintures] = useState([]);
+    useEffect(() => {
+        const findSeries = async() => {
+            try {
+               const series = await fetch (`https://api.themoviedb.org/3/search/tv?query=${content}&include_adult=false&language=en-US&page=1&api_key=${apikey}`);
+               const data = await series.json();
+               const date_names = data.results.map((tv) => ({ id: tv.id, name: tv.name }));
+               setSearchPintures(date_names);
+            } catch (error) {
+                console.error("Error fetching search data:", error);
+            }  
+        }; 
+        findSeries();
+    },[content]);
+
+    return searchPintures;
+}
+
+export function UseMediaSearch({content}){
+    const [mediaSearch, setMediaSearch] = useState([]);
+    useEffect(()=> {
+        const findMovies = UseMovieSearch({content}).slice(0,5);
+        const findSeries = UseSeriesSearch({content}).slice(0,5);
+        const combinedResults = [...findMovies, ...findSeries];
+        setMediaSearch(combinedResults);
+    },[content])
+    return mediaSearch;
+}
