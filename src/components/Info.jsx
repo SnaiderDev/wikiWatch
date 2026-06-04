0579461d20e600aea69ecfb922bb33a5e1d4245a
SnaiderDev <snaiderortega10@gmail.com>
2026-06-04 11:24:04 -0500

    Add uknown component detail

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,8 +1,6 @@
 import { useParams } from "react-router-dom";
 import { InfoDetails } from "./InfoDetails";
 
-
-
 export function InfoMovie({ id, type }) {
   return <InfoDetails id={id} type={type} />;
 }
@@ -11,6 +9,21 @@ export function InfoSeries({ id, type }) {
   return <InfoDetails id={id} type={type} />;
 }
 
+export function InfoUknown() {
+  return (
+    <main className="flex flex-col items-center gap-4 h-screen justify-center">
+      <article>
+        <h1 className="text-4xl font-bold">
+          <span className="text-[var(--color-primary)] ">Unknown </span>
+           media type
+        </h1>
+        <p className="text-lg">
+          The media type you are trying to access is not <span className="text-[var(--color-primary)] "> recognized.</span>
+        </p>
+      </article>
+    </main>
+  );
+}
 export function Info() {
   const { type, id } = useParams();
   return (
@@ -20,7 +33,7 @@ export function Info() {
       ) : type === "series" ? (
         <InfoSeries id={id} type={type} />
       ) : (
-        <>I don't know</>
+        <InfoUknown />
       )}
     </article>
   );
