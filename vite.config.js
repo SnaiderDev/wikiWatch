1042272af20cab0c31a5bd0c57a6a4091db84c05
SnaiderDev <snaiderortega10@gmail.com>
2026-06-12 09:14:39 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: 'https://github.io/wikiWatch/', 
+  base: 'https://SnaiderDev.github.io/wikiWatch', 
 })
